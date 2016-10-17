0bcc4473c41489c9b81f7715ac21c1b370b75c99
Kaden Liang Zhi Phua <kadenphua@gmail.com>
2016-10-17 10:06:00 +0800

    cant work

--- models/users.js
+++ models/users.js
@@ -1,12 +1,28 @@
 var mongoose = require('mongoose')
 
 // create a r for users model
+var childSchema = new mongoose.Schema({
+  name: String,
+  email: String
+})
 var usersSchema = new mongoose.Schema({
   // prop: Datatype
   name: String,
-  password: Number
+  password: Number,
+  email: String,
+  children: [ childSchema ]
 })
-
+var Children = mongoose.model('Children', childSchema)
+var children = new Children({
+  name: 'Prima',
+  email: 'prima@gmail.com'
+})
 var Users = mongoose.model('Users', usersSchema)
-
+users.children.push({name: 'Kailing', email: 'kailing@gmail.com'})
+children.save(function (err) {
+  if (err) console.log(err)
+})
+users.save(function (err) {
+  if (err) console.log(err)
+})
 module.exports = Users
